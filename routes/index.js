fd827dfc240df0d500619ce78dc4e681d1a3c89b
Jennie Zheng <jenniezheng321@gmail.com>
2017-09-08 11:33:40 -0700

    Hero

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var spawn = require('child_process').spawn
 
 router.get('/', function(req, res, next) {
 	//start.js
+	/*
     var py = spawn('python', ['python/call.py']),
     data = [1,2,3,4,5,6,7,8,9],
     dataString = '';
@@ -12,12 +13,12 @@ router.get('/', function(req, res, next) {
 	  dataString += data.toString();
 	});
 	py.stdout.on('end', function(){
-	  console.log('Sum of numbers=',dataString);
 	  res.render('index', { title: 'Express' , result: dataString});
 	});
 	py.stdin.write(JSON.stringify(data));
 	py.stdin.end();
-
+	*/
+	res.render('index', { title: 'Express' , result: dataString});
 });
 
 module.exports = router;
