0aae29982f30217a79afbcd4d82165585ab59593
Jennie Zheng <jenniezheng321@gmail.com>
2017-09-08 12:37:48 -0700

    flush

--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,11 @@ router.get('/', function(req, res, next) {
 	console.log("STARTING CHILD")
     var py = spawn('python', ['python/call.py']),
     data = [1,2,3,4,5,6,7,8,9],
-    dataString = '123';
+    dataString = '0';
 	py.stdout.on('data', function(data){
-		console.log( data.toString())
-	   //dataString += data.toString();
+	   dataString += data.toString();
 	});
 	py.stdout.on('end', function(){
-	  console.log("Child ENDED")
 	  res.render('index', { title: 'Express' , result: dataString});
 	});
 	py.stdin.write(JSON.stringify(data));
