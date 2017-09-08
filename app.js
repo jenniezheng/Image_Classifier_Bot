c3477ec7fa082b598ef64a51e06eb62ce83ea66a
Jennie Zheng <jenniezheng321@gmail.com>
2017-09-08 10:48:11 -0700

    heroku

--- app.js
+++ app.js
@@ -49,7 +49,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(3000,function(){
+app.listen(process.env.PORT, process.env.IP,function(){
   console.log("App started on localhost:"+3000);
 });
 
