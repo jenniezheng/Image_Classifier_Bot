b01d4efdd305f5c179678afd506d61f298b7f16e
Jennie Zheng <jenniezheng321@gmail.com>
2017-09-08 11:55:56 -0700

    WTF

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,10 @@ var router = express.Router();
 var spawn = require('child_process').spawn
 
 router.get('/', function(req, res, next) {
-    /*
+
     var py = spawn('python', ['python/call.py']),
     data = [1,2,3,4,5,6,7,8,9],
     dataString = '';
-
 	py.stdout.on('data', function(data){
 	  dataString += data.toString();
 	});
@@ -16,8 +15,7 @@ router.get('/', function(req, res, next) {
 	});
 	py.stdin.write(JSON.stringify(data));
 	py.stdin.end();
-	*/
-	res.render('index', { title: 'Express' , result: 123});
+
 });
 
 module.exports = router;
